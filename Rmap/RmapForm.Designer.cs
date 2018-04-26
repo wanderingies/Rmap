fa38ba8dd6d4e570a722f2a34afda93e7f783bbe
wanderingies <wanderingies@wanderingies.com>
2018-04-26 11:07:52 +0800

    添加项目文件。

---
+++ Rmap/RmapForm.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace Rmap
+{
+    partial class RForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CoorX";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox_DoubleClick);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.DoubleClick += new System.EventHandler(this.textBox_DoubleClick);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CoorY";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 21);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.DoubleClick += new System.EventHandler(this.textBox_DoubleClick);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Light";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(47, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(110, 21);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.DoubleClick += new System.EventHandler(this.textBox_DoubleClick);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Width";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(47, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(110, 21);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.DoubleClick += new System.EventHandler(this.textBox_DoubleClick);
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hight";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 165);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GroupBox";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(243, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(188, 49);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(211, 124);
+            this.listBox.TabIndex = 12;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // RForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 183);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(427, 222);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(427, 222);
+            this.Name = "RForm";
+            this.Text = "Rmap";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Rmap/RmapForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rmap
+{
+    public partial class RForm : Form
+    {
+        public RForm()
+        {
+            InitializeComponent();
+        }
+
+        int verion = 0;
+        float width = 0;
+        float length = 0;
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsNumber(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" && textBox2.Text == "" && textBox3.Text == "" && textBox4.Text == "" && textBox5.Text == "")
+                return;
+
+            if (listBox.Items.Count >= 3)
+                return;
+
+            string value = textBox1.Text + ";" + textBox2.Text + ";" + textBox3.Text + ";" + textBox4.Text + ";" + textBox5.Text;
+
+            if (listBox.Items.Contains(value))
+                return;
+
+            if (currentSelectedIndex != -1)
+                listBox.Items.RemoveAt(currentSelectedIndex);
+
+            listBox.Items.Add(value);
+            currentSelectedIndex = -1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "wmap|*.wmap";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (Stream stream = File.OpenRead(openFileDialog.FileName))
+                    {
+                        using (BinaryReader binaryReader = new BinaryReader(stream))
+                        {
+                            verion = binaryReader.ReadInt32();
+                            width = binaryReader.ReadSingle();
+                            length = binaryReader.ReadSingle();
+
+                            int count = binaryReader.ReadInt32();
+
+                            for (int i = 0; i < count; i++)
+                            {
+                                listBox.Items.Add(
+                                    binaryReader.ReadSingle().ToString() + ";" +
+                                    binaryReader.ReadSingle().ToString() + ";" +
+                                    binaryReader.ReadSingle().ToString() + ";" +
+                                    binaryReader.ReadSingle().ToString() + ";" +
+                                    binaryReader.ReadSingle().ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (verion == 0 && width == 0 && length == 0) return;
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "wmap|*.wmap";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using(Stream stream = File.Create(saveFileDialog.FileName))
+                    {
+                        using (BinaryWriter binaryWriter = new BinaryWriter(stream))
+                        {
+                            binaryWriter.Write(verion);
+                            binaryWriter.Write(width);
+                            binaryWriter.Write(length);
+
+                            for (int i = 0; i < listBox.Items.Count; i++)
+                            {
+                                string[] line = listBox.Items[i].ToString().Split(';');
+
+                                binaryWriter.Write(float.Parse(line[0]));
+                                binaryWriter.Write(float.Parse(line[1]));
+                                binaryWriter.Write(float.Parse(line[2]));
+                                binaryWriter.Write(float.Parse(line[3]));
+                                binaryWriter.Write(float.Parse(line[4]));
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex <= -1) return;
+
+            listBox.Items.RemoveAt(listBox.SelectedIndex);
+        }
+
+        private int currentSelectedIndex = -1;
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex <= -1) return;
+
+            currentSelectedIndex = listBox.SelectedIndex;
+            string[] line = listBox.SelectedItem.ToString().Split(';');
+
+            textBox1.Text = line[0];
+            textBox2.Text = line[1];
+            textBox3.Text = line[2];
+            textBox4.Text = line[3];
+            textBox5.Text = line[4];
+        }
+    }
+}
